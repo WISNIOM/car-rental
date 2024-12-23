9243805e89869e72dec35e0421ba4ff07a77769a
Marek Wiśniowski <mwisniowski@liveperson.com>
2024-12-24 00:02:09 +0100

    Added docker

--- apps/car-rental-backend/src/app/app.module.ts
+++ apps/car-rental-backend/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'password',
+      password: 'root',
       database: 'car_rental',
       entities: [],
       synchronize: true,
