b00c96e1588849a3db6088ea6717c0f95afef435
Marek Wiśniowski <mwisniowski@liveperson.com>
2024-12-30 15:04:04 +0100

    Added actions for elements

--- apps/car-rental/src/components/vehicles/vehicles-list/vehicles-list.component.ts
+++ apps/car-rental/src/components/vehicles/vehicles-list/vehicles-list.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VehiclesService } from '../../../services/vehicles.service';
 import { VehicleDto } from '../../../dtos/vehicle';
 import { VehicleBrandDto } from '../../../dtos/vehicle-brand';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { CreateVehicleFormComponent } from '../create-vehicle-form/create-vehicle-form.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-vehicles-list',
   imports: [
     CommonModule,
     MatTableModule,
+    MatMenuModule,
+    MatIconButton,
+    MatIconModule,
     MatPaginatorModule,
     MatDividerModule,
     CreateVehicleFormComponent,
@@ -20,19 +26,38 @@ import { MatPaginatorModule } from '@angular/material/paginator';
   templateUrl: './vehicles-list.component.html',
   styleUrl: './vehicles-list.component.css',
 })
-export class VehiclesListComponent implements OnInit {
+export class VehiclesListComponent implements OnInit, AfterViewInit {
   vehicles: VehicleDto[] = [];
   vehicleBrands: VehicleBrandDto[] = [];
   displayedColumns: string[] = [
     'brand',
     'registrationNumber',
     'vehicleIdentificationNumber',
+    'customerEmail',
+    'customerAddress',
+    'actions',
   ];
+  dataSource = new MatTableDataSource<VehicleDto>(this.vehicles);
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
 
   constructor(private readonly vehiclesService: VehiclesService) {}
 
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+  }
+
   ngOnInit(): void {
     this.loadVehicles();
+  }
+
+  editVehicle(vehicle: any): void {
+    // Implement edit vehicle logic here
+    console.log('Edit vehicle:', vehicle);
+  }
+
+  removeVehicle(vehicle: any): void {
+    // Implement remove vehicle logic here
+    console.log('Delete vehicle:', vehicle);
   }
 
   loadVehicles(): void {
